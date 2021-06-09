ff260c820ef35da50c98a1b45cf3d955aac7a86f
Tbakhm34 <tolganai_erlan_@mail.ru>
2021-06-09 09:11:20 +0000

    some more

--- deployments/docker/app.py
+++ deployments/docker/app.py
@@ -1,14 +1,16 @@
 from flask import Flask,  jsonify 
 import os 
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return jsonify({
         'message': 'hello world',
         'environment': os.environ.get('ENVIRONMENT'),
-        'owner': 'tolganay'
+        'owner': 'tolganay',
         'namespace': os.environ.get('NAMESPACE')
     })
+    
 @app.route('/soon')
 def comming_soon():
     return jsonify({
